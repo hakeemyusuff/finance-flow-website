fbd3412ddc177eb6669ae664ac9a7c0a1a217b82
hakeemyusuff <hakeemyusuff19@gmail.com>
2023-07-30 18:01:45 +0100

    completed home-page styling and linked all pages

---
+++ Javascripts/main.js
@@ -0,0 +1,43 @@
+const headerNav = document.querySelector(".header-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  headerNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("Aria-expanded", false)
+    : navToggle.setAttribute("Aria-expanded", true);
+  headerNav.toggleAttribute("data-visible");
+});
+
+/* swiper */
+// core version + navigation, pagination modules:
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  spaceBetween: 5,
+
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    700: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
